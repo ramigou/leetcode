6a4779274c538b4c553ebcf1708c2745fda56aeb
ramigou <dasul1021@gmail.com>
2023-12-06 07:20:05 +0900

    refactor: edit file name and remove TreeNode code

--- tree/Solution111.java
+++ tree/Solution111.java
@@ -1,27 +1,7 @@
 package tree;
 
-class Solution {
-    // Definition for a binary tree node.
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {}
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
+class Solution111 {
     int answer = Integer.MAX_VALUE;
-
 
     public int minDepth(TreeNode root) {
         if (root == null)
